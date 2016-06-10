f31866613579a95e36d5ad5af8e1ad7c6cef14d4
Szük László Dániel <daniel.szuk@gmail.com>
2016-06-10 13:31:15 +0200

    get_names finished, alg.js finished

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Get the cities and towns of Hungray names, and latitude, longitude bounds
 
 #Requirements
 1. You need node.js installed on your system ([Node.js](https://nodejs.org))
+
+#Usage
+1. Run the alg.js file (`node alg`)
+2. The towns names and datas saved to **dist/towns.js**
---
+++ alg.js
@@ -0,0 +1,20 @@
+/*jslint node: true, nomen: true*/
+"use strict";
+
+//If the population number below of popLimit, the town will be ignored
+var popLimit = 3000;
+
+var get_names = require('./get_names'),
+    fs = require('fs'),
+    towns,
+    file;
+
+towns = get_names(popLimit);
+
+file = fs.createWriteStream('dist/towns.js');
+file.write("towns=['");
+file.write(towns.join("','"));
+file.write("']");
+file.end();
+
+console.log(towns.length + " towns data succesfully writed to dist/towns.js");
